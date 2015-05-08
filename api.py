092b5c46698f0f3e2168cce60d306802824dbb54
Amanda Gilmore <ms.amgilmore@gmail.com>
2015-05-08 14:37:02 -0700

    Refactored the API call to be more dynamic so the program can expand its search parameters if there aren't geocoords for a given address. Logic check for whether the API returns usable results is still in progress.

--- api.py
+++ api.py
@@ -15,18 +15,12 @@ API_KEY = os.environ['GEOCODING_API_KEY']
 
 BASE_URL = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
-def get_geocoords(address, city, state):
-	"""Function should take API parameters as strings and return a tuple 
+def find_geocoords(address):
+	"""Function should take an address object and return a tuple 
 	with the latitude and longitude of that address. If none is found,
 	the coordinates of the city and state should be returned."""
 
-	encoded_parameters = quote_plus(address + city + state)
-	api_request = BASE_URL + "address=" + encoded_parameters + "&key=" + API_KEY
-
-	response = requests.get(api_request)
-	# response_string = str(response.content)
-	dict_response = json.loads(response.content)
-	print dict_response
+	dict_response = call_api(address=address.name, city=address.city_name, state=address.state_abbreviation)
 
 	# Check that we have results before extracting coords:
 	if dict_response['status'] == 'OK':
@@ -37,9 +31,35 @@ def get_geocoords(address, city, state):
 
 	# If no results, call API again for just city and state
 	elif dict_response['status'] == 'ZERO_RESULTS':
-		# TODO write a helper function to construct the API request and return the 
-		# response as a Python dict for this logical check.
+		dict_response = call_api(city=address.city_name, state=address.state_abbreviation)
+	elif:
+		dict_response = call_api(state=address.state)
+
+	else:
+		# In the unlikely event there aren't geocoords for the state, return hardcoded coords
+		# for USA. In production, I might instrument this to notify someone that the
+		# geocoord API might be down or have another issue requiring an engineer's attention.
 		return (0,0)
+
+
+def call_api(**kwargs):
+	"""Function to fetch geocoordinates for any combination of parameters accepted
+	by the Google Geocoding API."""
+
+	# Put args in the right order for the API:
+
+
+
+	encoded_parameters = quote_plus()
+	api_request = BASE_URL + "address=" + encoded_parameters + "&key=" + API_KEY
+
+	response = requests.get(api_request)
+	# response_string = str(response.content)
+	dict_response = json.loads(response.content)
+	print dict_response
+
+	return response_as_dict
+
 
 
 def main():
